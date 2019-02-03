da52ecceafde371e9ccadeae2fd72a9d9270eea4
Aquilla Sherrock <acsherrock@gmail.com>
2019-02-03 15:07:41 -0600

    Server running

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import * as express from "express";
+const app = express();
+const port = 8080; // default port to listen
+
+// Configure Express to use EJS
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    // render the index template
+    res.send("hi");
+} );
+
+// start the express server
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+} );
