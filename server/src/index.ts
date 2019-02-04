cfd2a0ca496dd7efb4831c44ad64381e17396165
Aquilla Sherrock <acsherrock@gmail.com>
2019-02-04 16:42:06 -0600

    adding client dist directory

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,12 @@ import * as express from "express";
 const app = express();
 const port = 8080; // default port to listen
 
-app.get( "/", ( req, res ) => {
+app.get("/", (req, res) => {
     // render the index template
     res.send("hi");
-} );
+});
 
 // start the express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}`);
+});
