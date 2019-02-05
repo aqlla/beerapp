8a5e8d846e5c2ff493ddaf5b730114ceeab204ca
Aquilla Sherrock <acsherrock@gmail.com>
2019-02-04 19:31:17 -0600

    ugh

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,17 +3,14 @@ import * as path from "path";
 const app = express();
 const port = 8080; // default port to listen
 
-
-
 app.get("/", (req, res) =>
-    res.sendFile(path.join(__dirname + '../../client/index.html')));
+    res.sendFile(path.join(__dirname + "../../client/index.html")));
 
 app.get("/api", (req, res) =>
     res.send("hi"));
 
-app.get('/api/beer', (req, res) => {
-
-});
+app.get("/api/beer", (req, res) =>
+    res.send("beers");
 
 // start the express server
 app.listen(port, () => {
