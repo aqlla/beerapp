71c2a5ca9ca2f2c6f0b7f1902e60ab3d75818c98
Aquilla Sherrock <acsherrock@gmail.com>
2019-02-04 19:41:31 -0600

    ugh

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,15 @@
 import * as express from "express";
 import * as path from "path";
+import { Client } from "pg";
+
+const pg = new Client({
+    user: 'acs',
+    database: 'beer'
+});
 const app = express();
-const port = 8080; // default port to listen
+const port = 8080;
+
+
 
 app.get("/", (req, res) =>
     res.sendFile(path.join(__dirname + "../../client/index.html")));
@@ -9,10 +17,10 @@ app.get("/", (req, res) =>
 app.get("/api", (req, res) =>
     res.send("hi"));
 
-app.get("/api/beer", (req, res) =>
-    res.send("beers"));
-
-// start the express server
-app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
+app.get("/api/beer", async (req, res) => {
+    await pg.connect();
+    res.send(await pg.query('select * from beers'));
 });
+
+app.listen(port, (_) =>
+    console.log(`server started at http://localhost:${port}`));
