94f89021d157d60246c10a2a95ed0502a6d5405a
Aquilla Sherrock <acsherrock@gmail.com>
2019-02-04 20:25:38 -0600

    added search

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,18 +12,39 @@ const port = 8080;
 app.get("/", (req, res) =>
     res.sendFile(path.join(__dirname + "../../client/index.html")));
 
-app.get("/api", (req, res) =>
-    res.send("hi"));
+app.get("/api", (req, res) => res.send("hi"));
 
 app.get("/api/beer", async (req, res) => {
     try {
         await pg.connect();
-        res.send(await pg.query("select * from beers"));
+        const data = await pg.query("select * from beers");
+        if ("rows" in data) {
+            res.send(data.rows);
+        }
         await pg.end();
     } catch (e) {
         console.log(e);
     }
+});
 
+app.get("/api/beer/:field/:val", async (req, res) => {
+    const fields = [ "style", "brewery" ];
+    if (req.params.field in fields) {
+        try {
+            await pg.connect();
+            const query = `select * from beers where ${req.params.field}=${req.params.val}`;
+            const data = await pg.query(query);
+            if ("rows" in data) {
+                res.send(data.rows);
+            }
+            await pg.end();
+        } catch (e) {
+            res.send(e);
+            console.log(e);
+        }
+    } else  {
+        res.send(`${req.params.field} is not a valid search field`);
+    }
 });
 
 app.listen(port, (_) =>
