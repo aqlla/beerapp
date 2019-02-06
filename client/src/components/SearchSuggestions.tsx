ff25e743bcf1714d5fda159e7338dfb6375a4e7e
Aquilla Sherrock <acsherrock@gmail.com>
2019-02-05 21:06:52 -0600

    pretty sure it'll work this time

--- client/src/components/SearchSuggestions.tsx
+++ client/src/components/SearchSuggestions.tsx
@@ -5,7 +5,7 @@ export interface ISearchSuggestionsOptions {
 }
 
 const SearchSuggestions = (options: ISearchSuggestionsOptions) => {
-    if (options && options.results && options.results.hasOwnProperty("map")) {
+    if (options && options.results && options.results.hasOwnProperty("map") && typeof options.results.map === "function") {
         const suggestions = options.results.map(r => (
             <li key={r.id}>
                 {r.name}
